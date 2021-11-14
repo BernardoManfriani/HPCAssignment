eb74c493eab15d0fe318c00cfdd49794046f6427
Bernardo Manfriani <berna@ct1pt-tnode010.hpc>
2021-11-14 17:44:28 +0100

    new benchmarks

--- section2Script.sh
+++ section2Script.sh
@@ -1,31 +1,43 @@
-#!/bin/bash
-#PBS -l nodes=1:ppn=2
-#PBS -q dssc
+#!/bin/bash 
 
-cd mpi_examples/mpi-benchmarks/src_c 
+#PBS -l nodes=2:ppn=24 
 
-module load openmpi-4.1.1+gnu-9.3.0
+#PBS -l walltime=00:01:00 
 
-echo -e $"\n \n \n########################################## \n"
-echo -e $"######## 1 NODE 1 SOCKET 2  CORES ######## \n" 
-echo -e $"########################################## \n \n \n \n"
-mpirun -np 2 --map-by core --report-bindings ./IMB-MPI1 PingPong 2> /dev/null | grep -v ^# | grep -v -e '^$' |  tr -s ' '| sed 's/^[ \t]+*//g' | sed 's/[ \t]+*/,/g' >oneNodeOneSocket.csv 
+#PBS -q dssc 
 
+  
 
-echo -e $"\n \n \n########################################## \n"
-echo -e $"######## 1 NODE 2 SOCKET 2  CORES ######## \n"
-echo -e $"########################################## \n \n \n \n"
-mpirun -np 2 --map-by socket --report-bindings ./IMB-MPI1 PingPong 2> /dev/null | grep -v ^# | grep -v -e '^$' |  tr -s ' '| sed 's/^[ \t]+*//g' | sed 's/[ \t]+*/,/g' >oneNodeTwoSocket.csv 
+cd $PBS_O_WORKDIR 
 
-echo -e $"\n \n \n########################################## \n"
-echo -e $"######## 2 NODE 2 SOCKET 2  CORES ######## \n"
-echo -e $"########################################## \n \n \n \n"
-mpirun -np 2 --map-by node --report-bindings ./IMB-MPI1 PingPong 2> /dev/null | grep -v ^# | grep -v -e '^$' |  tr -s ' '| sed 's/^[ \t]+*//g' | sed 's/[ \t]+*/,/g' >twoNodeTwoSocket.csv 
+module load openmpi-4.1.1+gnu-9.3.0  
 
-# mpirun -np 2 --map-by node --report-bindings ./IMB-MPI1 PingPong     
+ 
 
-# mpirun -np 2 --map-by node --report-bindings ./IMB-MPI1 PingPong
+mpirun -mca pml ob1 --mca btl self,vader --report-bindings -np 2 --map-by core ./IMB-MPI1 PingPong 2> /dev/null | grep -v ^# | grep -v -e '^$' |  tr -s ' '| sed 's/^[ \t]+*//g' | sed 's/[ \t]+*/,/g' >bycore_vader.csv 
 
-# mpirun -np 2 --map-by node --report-bindings ./IMB-MPI1 PingPong     
+mpirun -mca pml ob1 --mca btl self,tcp --report-bindings -np 2 --map-by core ./IMB-MPI1 PingPong 2> /dev/null | grep -v ^# | grep -v -e '^$' |  tr -s ' '| sed 's/^[ \t]+*//g' | sed 's/[ \t]+*/,/g' >bycore_tcp.csv 
 
-exit
+mpirun -mca pml ucx --report-bindings -np 2 --map-by core ./IMB-MPI1 PingPong 2> /dev/null | grep -v ^# | grep -v -e '^$' |  tr -s ' '| sed 's/^[ \t]+*//g' | sed 's/[ \t]+*/,/g' >bycore_ucx.cvs 
+
+mpirun -mca pml ob1 --mca btl self,tcp --report-bindings -np 2 --map-by socket ./IMB-MPI1 PingPong 2> /dev/null | grep -v ^# | grep -v -e '^$' |  tr -s ' '| sed 's/^[ \t]+*//g' | sed 's/[ \t]+*/,/g' >bysocket_tcp.csv 
+
+mpirun -mca pml ucx --report-bindings -np 2 --map-by socket ./IMB-MPI1 PingPong 2> /dev/null | grep -v ^# | grep -v -e '^$' |  tr -s ' '| sed 's/^[ \t]+*//g' | sed 's/[ \t]+*/,/g' >bysocket_ucx.csv 
+
+mpirun -mca pml ob1 --mca btl self,vader --report-bindings -np 2 --map-by socket ./IMB-MPI1 PingPong 2> /dev/null | grep -v ^# | grep -v -e '^$' |  tr -s ' '| sed 's/^[ \t]+*//g' | sed 's/[ \t]+*/,/g' >by_socket_vader.csv 
+
+mpirun -mca pml ob1 --mca btl self,tcp --report-bindings -np 2 --map-by core ./IMB-MPI1 PingPong 2> /dev/null | grep -v ^# | grep -v -e '^$' |  tr -s ' '| sed 's/^[ \t]+*//g' | sed 's/[ \t]+*/,/g' >bynode_tcp.csv 
+
+mpirun -mca pml ucx  --report-bindings -np 2 --map-by core ./IMB-MPI1 PingPong 2> /dev/null | grep -v ^# | grep -v -e '^$' |  tr -s ' '| sed 's/^[ \t]+*//g' | sed 's/[ \t]+*/,/g' >bynode_ucx.csv 
+
+  
+
+git add * 
+
+git commit -m "new benchmarks" 
+
+git push 
+
+  
+
+exit 
