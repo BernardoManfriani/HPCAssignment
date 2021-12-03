493f0fc74ee5484f621a0ad39f3365fc48c5cc4a
Bernardo Manfriani <berna@login.hpc>
2021-12-03 16:35:49 +0100

    add script

--- section1/section1Script.sh
+++ section1/section1Script.sh
@@ -6,9 +6,16 @@
 cd $PBS_O_WORKDIR
 module load openmpi-4.1.1+gnu-9.3.0
 
+time_first=[]
+
 for x in {2..48}
 do
-  mpirun -np $x --mca btl ^openib ./ringNonBlocking.x > ring_time_non_blocking_${x}_procc  
+   mpirun -np $x --mca btl ^openib ./ringNonBlocking.x > ring_time_non_blocking_${x}_procc  
+  
+   str=$(cat output_ring_non_blocking_${x}_procc | tail -1 | cut -f2 -d '#######Execution time ')
+ 
+   time_first[x]=$(echo ${str} | cut -d ' ' -f1)
+   printf '%d\t%s\t\t%s\n' ${x} ${time_first[x]} >> results.csv   
 done
 
 
