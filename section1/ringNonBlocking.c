d4da3e60b0d78f0137e408cfc4e3aba50f03ea9b
BernardoManfriani <bernardomanfriani@gmail.com>
2021-12-07 17:08:55 +0100

    Update ringNonBlocking.c

--- section1/ringNonBlocking.c
+++ section1/ringNonBlocking.c
@@ -15,6 +15,7 @@ int main (int argc, char * argv[])
   double timeS;  //Start Time
   double timeE;  //End Time
   double timeT = 0;
+  double timeF = 0;
   double timeRcv;
 
   MPI_Request reqs[4];   // required variable for non-blocking calls
@@ -24,7 +25,7 @@ int main (int argc, char * argv[])
   MPI_Init( &argc, &argv );
   MPI_Barrier(MPI_COMM_WORLD);
 
-  for (size_t i = 0; i < 100; i++) {
+
 
     MPI_Comm_size( MPI_COMM_WORLD,&size );
     MPI_Cart_create( MPI_COMM_WORLD, 1, &size, &qperiod, 1, &ringCommunicator );    //left and right processors
@@ -40,34 +41,34 @@ int main (int argc, char * argv[])
     status[2].MPI_TAG = 1;  //Initialise status tag for have safe check in the while loop
     status[3].MPI_TAG = 1;  //Initialise status tag for have safe check in the while loop
 
-    for (size_t i = 0; i < 10000; i++) {
-      timeS = MPI_Wtime();
-      while(status[3].MPI_TAG != rank*10 && status[2].MPI_TAG != rank*10){
-        MPI_Isend(&msgleft, 1, MPI_INT, leftP, itag1, ringCommunicator, &reqs[0]);          //Send msglefto left
-        MPI_Isend(&msgright, 1, MPI_INT, rightP, itag2, ringCommunicator, &reqs[1]);        //Send msright to right
-        MPI_Irecv(&msgFromRight, 1, MPI_INT, rightP, MPI_ANY_TAG, ringCommunicator, &reqs[2]); //Receive msgrcv[1] from right
-        MPI_Irecv(&msgFromLeft, 1, MPI_INT, leftP, MPI_ANY_TAG, ringCommunicator, &reqs[3]);  //Receive msgrcv[0] from left
+      for (size_t i = 0; i < 10000; i++) {
+        timeS = MPI_Wtime();
+        while(status[3].MPI_TAG != rank*10 && status[2].MPI_TAG != rank*10){
+          MPI_Isend(&msgleft, 1, MPI_INT, leftP, itag1, ringCommunicator, &reqs[0]);          //Send msglefto left
+          MPI_Isend(&msgright, 1, MPI_INT, rightP, itag2, ringCommunicator, &reqs[1]);        //Send msright to right
+          MPI_Irecv(&msgFromRight, 1, MPI_INT, rightP, MPI_ANY_TAG, ringCommunicator, &reqs[2]); //Receive msgrcv[1] from right
+          MPI_Irecv(&msgFromLeft, 1, MPI_INT, leftP, MPI_ANY_TAG, ringCommunicator, &reqs[3]);  //Receive msgrcv[0] from left
 
-        np = np + 2;  //Updating number of time that rank processor has recived a message
-        MPI_Waitall(4, reqs, status);   //Wait for every call MPI_Irecv and MPI_Irecv to mantain safety
+          np = np + 2;  //Updating number of time that rank processor has recived a message
+          MPI_Waitall(4, reqs, status);   //Wait for every call MPI_Irecv and MPI_Irecv to mantain safety
 
-        msgleft = msgFromRight - rank ;    //received from right forward to left substracted
-        msgright = msgFromLeft + rank;    //received from left forward to right added
-        itag2 = status[3].MPI_TAG;      //taken received tags for forwarding and complete the ring
-        itag1 = status[2].MPI_TAG;      //taken received tags for forwarding and complete the ring
+          msgleft = msgFromRight - rank ;    //received from right forward to left substracted
+          msgright = msgFromLeft + rank;    //received from left forward to right added
+          itag2 = status[3].MPI_TAG;      //taken received tags for forwarding and complete the ring
+          itag1 = status[2].MPI_TAG;      //taken received tags for forwarding and complete the ring
+        }
+        MPI_Barrier(ringCommunicator);
+
+        timeE = MPI_Wtime();
+        printf("%10.8f\n", timeT);
+        timeT = timeE - timeS;
+        MPI_Reduce(&timeT, &timeRcv, 1, MPI_DOUBLE, MPI_MAX,0, ringCommunicator);
+        timeF = timeF + timeRcv;
       }
-
-      MPI_Barrier(ringCommunicator);
-      timeE = MPI_Wtime();
-      timeT = timeT + (timeE - timeS);
-    }
-
-
   }
   printf("I am process %d and I have received %d messages. My final messages have tag %d and value %d, %d\n", rank, np, status[2].MPI_TAG ,msgFromLeft, msgFromRight);
-  timeT = timeT / 100 ;
-  printf("%10.8f\n", timeT);
-  MPI_Reduce(&timeT, &timeRcv, 1, MPI_DOUBLE, MPI_MIN,0, ringCommunicator);
+  timeF = timeF / 10000 ;
+
 
   MPI_Finalize();
 
