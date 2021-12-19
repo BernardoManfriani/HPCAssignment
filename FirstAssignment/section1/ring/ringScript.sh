219e3ad4bc9f29db79a9b9c00c71de1bc70272ce
Bernardo Manfriani <bernardomanfriani@gmail.com>
2021-12-19 22:40:40 +0100

    some results

---
+++ FirstAssignment/section1/ring/ringScript.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#PBS -l nodes=2:ppn=24
+#PBS -l walltime=02:00:00
+#PBS -q dssc
+
+cd $PBS_O_WORKDIR 
+module load openmpi-4.1.1+gnu-9.3.0 
+
+time_first=[]
+rm results.csv
+rm ring_non_blocking*
+
+for x in {2..48}
+do
+   mpirun --map-by core -np $x --mca btl ^openib ./ringNonBlock.x > ring_non_blocking_${x}_procc
+
+   str=$(cat ring_non_blocking_${x}_procc | tail -1 )
+
+   time_first[x]=$(echo ${str} )
+   printf '%d\t%s\n' ${x} ${time_first[x]} >> results.csv
+done
+
+
+
+exit
+
