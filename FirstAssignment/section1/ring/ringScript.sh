9aa470e4503a6b45666d10bed628ecd8cffcfdbf
Bernardo Manfriani <bernardomanfriani@gmail.com>
2021-12-19 23:17:43 +0100

    usually

--- FirstAssignment/section1/ring/ringScript.sh
+++ FirstAssignment/section1/ring/ringScript.sh
@@ -8,13 +8,13 @@ module load openmpi-4.1.1+gnu-9.3.0
 
 time_first=[]
 rm results.csv
-rm ring_non_blocking*
+rm ring_*
 
 for x in {2..48}
 do
-   mpirun --map-by core -np $x --mca btl ^openib ./ringNonBlock.x > ring_non_blocking_${x}_procc
+   mpirun --map-by core -np $x --mca btl ^openib ./ring.x > ring_${x}_procc
 
-   str=$(cat ring_non_blocking_${x}_procc | tail -1 )
+   str=$(cat ring_${x}_procc | tail -1 )
 
    time_first[x]=$(echo ${str} )
    printf '%d\t%s\n' ${x} ${time_first[x]} >> results.csv
