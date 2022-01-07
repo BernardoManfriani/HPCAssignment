ecb3aebf4031b736e5ba222d5c1812393232067e
BernardoManfriani <bernardomanfriani@gmail.com>
2022-01-07 11:33:07 +0100

    Update provaMP01.c

--- SecondAssignment/prove/provaMP01.c
+++ SecondAssignment/prove/provaMP01.c
@@ -12,20 +12,35 @@
 #include <omp.h>
 
 int main(int argc, char const *argv[]) {
-  int nthreads = 1;
+  //int nthreads = 1;
 
+  // #pragma omp parallel
+  // {
+  //    int myid = omp_get_thread_num();
+  //   #pragma omp master
+  //    int nthreads = omp_get_num_threads();
+  //   #pragma omp barrier
+  //
+  //   #pragma omp for ordered
+  //    for ( int i = 0; i < nthreads; i++ ){
+  //     #pragma omp ordered
+  //      printf( "greetings from thread num %d\n", myid );
+  //    }
+  // }
+
+  int order = 0;
   #pragma omp parallel
   {
-     int myid = omp_get_thread_num();
-    #pragma omp master
-     int nthreads = omp_get_num_threads();
-    #pragma omp barrier
-
-    #pragma omp for ordered
-     for ( int i = 0; i < nthreads; i++ ){
-      #pragma omp ordered
-       printf( "greetings from thread num %d\n", myid );
-     }
+    int myid = omp_get_thread_num();
+    int done = 0
+    while (!done) {
+     #pragma omp critical
+      if ( order == myid ) {
+        printf( "greetings from thread num %d\n", myid );
+        order++;
+        done=1;
+      }
+    }
   }
   return 0;
 }
