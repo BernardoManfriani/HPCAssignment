efdcf935f7ab5ba13408846b311a3fea16cffbe2
BernardoManfriani <bernardomanfriani@gmail.com>
2022-01-07 11:25:05 +0100

    Update provaMP01.c

--- SecondAssignment/prove/provaMP01.c
+++ SecondAssignment/prove/provaMP01.c
@@ -13,12 +13,14 @@
 
 int main(int argc, char const *argv[]) {
   int nthreads = 1;
+  
   #pragma omp parallel
   {
      int myid = omp_get_thread_num();
     #pragma omp master
      int nthreads = omp_get_num_threads();
     #pragma omp barrier
+
     #pragma omp for ordered
      for ( int i = 0; i < nthreads; i++ ){
       #pragma omp ordered
