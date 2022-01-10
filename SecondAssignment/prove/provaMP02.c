8451fd49fe79f5765360f039c37564d8f594629a
BernardoManfriani <bernardomanfriani@gmail.com>
2022-01-10 10:58:23 +0100

    Update provaMP02.c

--- SecondAssignment/prove/provaMP02.c
+++ SecondAssignment/prove/provaMP02.c
@@ -14,18 +14,21 @@
 int main(int argc, char const *argv[]) {
     double PI = 3.1415;
     int morning_coffees = MAX_INTEGER;
-    char password[] = “dont_ask_dont_tell”
-    int final_mark
+    char password[] = “dont_ask_dont_tell”;
+    int final_mark;
 
     #pragma omp parallel firstprivate(PI, morning_coffees) private(password){
       drink_mycoffees( morning_coffees );
       use_pi( PI );
       password = setup_mypassword();
       int exam_passed = 0;
-      while (!exam_passed) { exam_passed = try() }
-      #pragma omp for lastprivate( final_mark)
-      for( int i; i < nthreads; i++ )
-      final_mark = exam_passed;
+      while (!exam_passed) {
+        exam_passed = try()
+      }
+    #pragma omp for lastprivate( final_mark)
+      for( int i; i < nthreads; i++ ){
+        final_mark = exam_passed;
+      }
     }
 
   return 0;
