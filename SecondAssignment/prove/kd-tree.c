ec1518d8efa9e8d18645b23e031598a0034f2ced
BernardoManfriani <bernardomanfriani@gmail.com>
2022-01-18 19:14:23 +0100

    Update kd-tree.c

--- SecondAssignment/prove/kd-tree.c
+++ SecondAssignment/prove/kd-tree.c
@@ -10,22 +10,22 @@
 #     define _XOPEN_SOURCE 700
 #  endif
 #endif
-// #if !defined(DOUBLE_PRECISION)
-// # define float_t float
-// #else
-// # define float_t double
-// #endif
-#define NDIM 2
-// #define float_t double
-struct kpoint {
-  int[NDIM];
-};
-
-struct kdnode {
-  int axis; // the splitting dimension
-  kpoint split; // the splitting element
-  struct kdnode *left, *right; // the left and right sub-trees
-};
+// // #if !defined(DOUBLE_PRECISION)
+// // # define float_t float
+// // #else
+// // # define float_t double
+// // #endif
+// #define NDIM 2
+// // #define float_t double
+// struct kpoint {
+//   int[NDIM];
+// };
+//
+// struct kdnode {
+//   int axis; // the splitting dimension
+//   kpoint split; // the splitting element
+//   struct kdnode *left, *right; // the left and right sub-trees
+// };
 
 int main(int argc, char const *argv[]) {
   int array_0[10];
