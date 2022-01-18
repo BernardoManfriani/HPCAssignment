140a8ec1fe019d2343c5d4e9916db9b54e52a175
BernardoManfriani <bernardomanfriani@gmail.com>
2022-01-18 18:53:18 +0100

    Create kd-tree.c

---
+++ SecondAssignment/prove/kd-tree.c
@@ -0,0 +1,32 @@
+#if defined(__STDC__)
+#  if (__STDC_VERSION__ >= 199901L)
+#     define _XOPEN_SOURCE 700
+#  endif
+#endif
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+#include <math.h>
+#include <omp.h>
+
+#if !defined(DOUBLE_PRECISION)
+#define float_t float
+#else
+#define float_t double
+#endif
+#define NDIM 2
+
+struct kpoint float_t[NDIM];
+
+struct kdnode {
+int axis; // the splitting dimension
+kpoint split; // the splitting element
+struct kdnode *left, *right; // the left and right sub-trees
+}
+
+int main(int argc, char const *argv[]) {
+  int array_0[10];
+  return 0;
+}
